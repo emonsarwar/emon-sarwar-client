ab9ff1039639fbf2034253066b21e5d8d34ec85b
Emon Sarwar <emonsarwar111@gmail.com>
2026-02-22 05:46:55 +0600

    Live link added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,7 +13,7 @@ export default function Contact() {
   setLoading(true);
   console.log("Submitting form:", form);  
   try {
-    const res = await fetch("http://localhost:5000/api/contact", {
+    const res = await fetch("https://emon-sarwar-client.vercel.app/api/contact", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(form)
